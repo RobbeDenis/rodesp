4827b1335d74c59e186e713805ef516c2d4a7f48
RobbeDenis <77392406+RobbeDenis@users.noreply.github.com>
2026-04-27 19:29:53 +0200

    Added ValidWave concept
    - GeneratBlock can only be used with a valid wave.
    - ValidWave only checks for has_float_implementation.
    - CosinefWave has been added.

--- Generators.hpp
+++ Generators.hpp
@@ -7,16 +7,27 @@
 
 namespace rodesp::gen
 {
+	namespace detail
+	{
+		template<typename T>
+		concept has_float_implementation = requires(T v, float phase){
+			{ v(phase) } -> std::same_as<float>;
+		};
+	}
+
+	template<typename T>
+	concept ValidWave = detail::has_float_implementation<T>;
+
 	struct GenerateArgs
 	{
 		float SampleRate{ 0.f };
 		float Frequency{ 0.f };
 		float Amplitude{ 1.f };
-		std::span<float> View;
+		std::span<float> Buffer;
 	};
 
-	// functor used to populate a float buffer based on the WavePolicy
-	template<typename WavePolicy>
+	// Functor used to populate a float buffer based on the Wave
+	template<ValidWave Wave>
 	struct GenerateBlock
 	{
 		inline void operator()(const GenerateArgs& args)
@@ -24,22 +35,30 @@ namespace rodesp::gen
 			const float deltaPhase{ 1.f / args.SampleRate * args.Frequency };
 			Phase = exp::FastWrapPhase(Phase);
 
-			for (auto& sample : args.View)
+			for (auto& sample : args.Buffer)
 			{
 				sample = args.Amplitude * Generate(Phase);
 				Phase = exp::FastWrapPhase(Phase + deltaPhase);
 			}
 		}
 
-		WavePolicy Generate{ };
+		Wave Generate{ };
 		float Phase{ 0.f };
 	};
 
-	struct SineWave
+	struct SinefWave
 	{
 		inline float operator()(float phase)
 		{
 			return std::sinf(numbers::pi2_v<float> * phase);
+		}
+	};
+
+	struct CosinefWave
+	{
+		inline float operator()(float phase)
+		{
+			return std::cosf(numbers::pi2_v<float> * phase);
 		}
 	};
 }
