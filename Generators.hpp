b6fa098a205043f09649105083b6006eed021351
RobbeDenis <77392406+RobbeDenis@users.noreply.github.com>
2026-04-27 02:44:35 +0200

    Added base structs
    Added base structs with intention to test  compiler inline behaviour between functor and function

--- Generators.hpp
+++ Generators.hpp
@@ -1,1 +1,46 @@
 #pragma once
+
+#include <cmath>
+
+#include "Rodesp.hpp"
+
+namespace rodesp::gen
+{
+	struct GenerateArgs
+	{
+		// freq, phase, span of buffer?
+	};
+
+	// test dif between functor GenerateBlock and inline function GenerateBlock
+	template<typename WavePolicy>
+	struct GenerateBlock
+	{
+		inline void operator()(const GenerateArgs& args)
+		{
+
+		}
+
+		WavePolicy Generate;
+		float Phase
+	};
+
+	template<typename WavePolicy>
+	struct BlockGenerator
+	{
+		inline void GenerateBlock(const GenerateArgs& args)
+		{
+
+		}
+
+		WavePolicy Generate;
+		float Phase
+	};
+
+	struct SineWave
+	{
+		inline float operator()(float phase)
+		{
+			return std::sin(numbers::pi2_v<float> * phase);
+		}
+	};
+}
--- Rodesp.hpp
+++ Rodesp.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <numbers>
 #include <concepts>
 #include <smmintrin.h>
